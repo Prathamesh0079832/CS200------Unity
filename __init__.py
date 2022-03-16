698afc89100ca1cc0b868701a6699364510b22c6
Prat <ram.prathamesh@gmail.com>
2022-03-16 14:31:52 +0530

    p: Added function for the questions page

--- __init__.py
+++ __init__.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return render_template('')
+    return render_template('home.html')
+
+@app.route('/quiz-on-git')
+def question_page():
+    global name
+    if request.form["Name"] == '':
+        name = 'Poweruser!'
+    else:
+        name = request.form["Name"]
+    return render_template('', username = name)
 
 if __name__ == "__main__":
     app.run(debug=True)
